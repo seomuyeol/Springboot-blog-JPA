61d576c5b95fac7d74cb8d275350c46713117891
seomy <muyear.seo@gmail.com>
2022-08-16 20:00:34 +0900

    delete

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,9 +67,20 @@ public class DummyControllerTest {
 		user.setEmail(requestUser.getEmail());
 		
 		//userRepository.save(user);
-		return null;
+		return user;
 	}
-
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "失敗しました、このidはないです。";
+		}
+		
+		
+		return  "id: " + id + "は削除されました。";
+	}
+	
 	@PostMapping("/dummy/join")
 	public String join(User user) {
 		System.out.println("username: " + user.getUsername());
